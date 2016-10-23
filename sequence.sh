ac7319a216f0dc6cb758ee09d0a7f4cbd2fd1258
Mike Rosseel <mike@eon-consult.be>
2016-10-23 21:41:35 +0200

    adding local jupyter

--- sequence.sh
+++ sequence.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd inputfiles/WWCra
 muniphot *.fit
-munimatch --verbose frame001.pht *.pht
+munimatch --verbose frame001.pht frame*.pht
 munilist -a 2 --object 1 -v 2 -c 3 out.txt match*.pht
 munifind -a 2 -c 0 out2.txt match00*
 cd ../..
