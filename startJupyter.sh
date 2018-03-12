f3c286e5c2e881fe6af2786fe435444c6fd031e5
Mike Rosseel <mike.rosseel@gmail.com>
2018-03-12 09:36:30 +0100

    change startup script so jupyter lab is started instead

--- startJupyter.sh
+++ startJupyter.sh
@@ -1,1 +1,1 @@
-docker run --user root -e GRANT_SUDO=yes -v $(pwd):/home/jovyan/work -d -p 8888:8888 miker/jupyter
+docker run --user root -e GRANT_SUDO=yes -v $(pwd):/home/jovyan/work -d -p 8888:8888 miker/jupyter start.sh jupyter lab
