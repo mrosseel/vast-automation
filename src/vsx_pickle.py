939569c3ef38d40324757ad30dffc81230d913d7
Mike Rosseel <mike.rosseel@gmail.com>
2019-07-05 12:16:36 +0200

    use fixed outputfile, replace as_matrix

--- src/vsx_pickle.py
+++ src/vsx_pickle.py
@@ -1,24 +1,27 @@
 import pickle
 import pandas as pd
 import argparse
-from init_loader import init, settings
-
+from init_base import Settings
 
 def convert(path):
+    print(f"Reading {path}...")
     #using pandas with a column specification
     col_specification =[(0, 7), (8, 38), (39, 40), (41,50), (51,60), (61,91), (93,94), (94,100),(100, 101), (101,107), (108,109), (109,110), (110,116), (116,117), (117,123), (124,136), (136,137), (138,139), (139,155), (155,158)]
     data = pd.read_fwf(path, colspecs=col_specification, names=('OID', 'Name','V','RAdeg', 'DEdeg', 'Type', 'l_max', 'max', 'u_max', 'n_max', 'f_min', 'l_min', 'min', 'u_min', 'n_min', 'Epoch', 'u_Epoch', 'l_Period', 'Period', 'u_Period'), converters={'l_max': str})
 
-    ra_deg_np = data['RAdeg'].as_matrix()
-    dec_deg_np = data['DEdeg'].as_matrix()
+    ra_deg_np = data['RAdeg'].values
+    dec_deg_np = data['DEdeg'].values
     metadata = []
     for index, row in data.iterrows():
         metadata.append({'id': index, 'OID': row['OID'], 'Name': row['Name'], 'Type': row['Type'], 'l_Period': row['l_Period'], 'Period': row['Period'], 'u_Period': row['u_Period']})
     vsx_dict = { 'ra_deg_np': ra_deg_np, 'dec_deg_np': dec_deg_np, 'metadata': metadata}
 
+    outfile = './' + Settings.vsx_catalog_name
+    print(f"Writing {outfile}...")
     # From Python 3.6 onwards, the standard dict type maintains insertion order by default. => no OrderedDict necessary
-    with open(settings.vsxcatalogdir, 'wb') as fp:
+    with open(outfile, 'wb') as fp:
         pickle.dump(vsx_dict, fp)
+    print(f"Done.")
 
 # returns:   { 'ra_deg_np': ra_deg_np, 'dec_deg_np': dec_deg_np, 'metadata': ({'id': index, 'OID': row['OID'], 'Name': row['Name'], 'Type': row['Type'], 'l_Period': row['l_Period'], 'Period': row['Period'], 'u_Period': row['u_Period']})}
 def read(path):
