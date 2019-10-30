989a6114e318b81f2b88db19731e9470ffa1d627
Mike Rosseel <mike.rosseel@gmail.com>
2019-10-30 22:21:13 +0100

    Add exception handling to get out of memory bug

--- src/do_charts_vast.py
+++ src/do_charts_vast.py
@@ -96,100 +96,112 @@ def plot_lightcurve(star_tuple: Tuple[StarDescription, DataFrame], chartsdir):
 
 
 def plot_lightcurve_jd(star: StarDescription, curve: DataFrame, chartsdir):
-    star_id = star.local_id
-    logging.debug(f"Plotting lightcurve for {star_id}")
-    vsx_name, separation, filename_no_ext = get_star_or_catalog_name(star, suffix=f"_light")
-    vsx_title = '' if vsx_name is None else f" ({vsx_name} - dist:{separation:.4f})"
-    save_location = PurePath(chartsdir, filename_no_ext+'.png')
-    start = timer()
-    upsilon_match = star.get_metadata('UPSILON') if star.has_metadata('UPSILON') else None
-    upsilon_text = upsilon_match.get_upsilon_string() if upsilon_match is not None else ''
-    end = timer()
-    logging.debug(f"timing upsilon stuff {end - start}")
-    coord = star.coords
-    # print(f'Plotting lightcurve with star_description:{star_description}, curve length:{len(curve)}, star:{star}, curve:{curve}')
-    if (curve is None):
-        logging.info(f"Curve is None for star {star_id}")
-        return
+    try:
+        star_id = star.local_id
+        logging.debug(f"Plotting lightcurve for {star_id}")
+        vsx_name, separation, filename_no_ext = get_star_or_catalog_name(star, suffix=f"_light")
+        vsx_title = '' if vsx_name is None else f" ({vsx_name} - dist:{separation:.4f})"
+        save_location = PurePath(chartsdir, filename_no_ext+'.png')
+        start = timer()
+        upsilon_match = star.get_metadata('UPSILON') if star.has_metadata('UPSILON') else None
+        upsilon_text = upsilon_match.get_upsilon_string() if upsilon_match is not None else ''
+        end = timer()
+        logging.debug(f"timing upsilon stuff {end - start}")
+        coord = star.coords
+        # print(f'Plotting lightcurve with star_description:{star_description}, curve length:{len(curve)}, star:{star}, curve:{curve}')
+        if (curve is None):
+            logging.info(f"Curve is None for star {star_id}")
+            return
 
-    curve_max = curve['realV'].max()
-    curve_min = curve['realV'].min()
-    fig = plt.figure(figsize=(20, 12), dpi=150, facecolor='w', edgecolor='k')
-    # plt.scatter(curve['floatJD'], curve['realV'])
-    plt.errorbar(curve['floatJD'], curve['realV'], yerr=curve['realErr'], linestyle='none', marker='o', ecolor='gray',
-                 elinewidth=1)
+        curve_max = curve['realV'].max()
+        curve_min = curve['realV'].min()
+        fig = plt.figure(figsize=(20, 12), dpi=150, facecolor='w', edgecolor='k')
+        # plt.scatter(curve['floatJD'], curve['realV'])
+        plt.errorbar(curve['floatJD'], curve['realV'], yerr=curve['realErr'], linestyle='none', marker='o', ecolor='gray',
+                     elinewidth=1)
 
-    plt.xlabel('JD', labelpad=TITLE_PAD)
-    # plt.ylabel("Absolute Mag, comp star = {:2.2f}".format(comparison_stars[0].vmag), labelpad=TITLE_PAD)
-    plot_max = curve_max + 0.1
-    plot_min = curve_min - 0.1
-    plt.ylim(plot_min, plot_max)
-    plt.xlim(curve['floatJD'].min(), curve['floatJD'].max())
-    plt.gca().invert_yaxis()
-    plt.ticklabel_format(style='plain', axis='x')
-    plt.title(f"Star {star_id}{vsx_title}\nposition: {utils.get_hms_dms_matplotlib(coord)}{upsilon_text}", pad=TITLE_PAD)
-    plt.tight_layout()
-    start = timer()
-    fig.savefig(save_location)
-    end = timer()
-    logging.debug(f"timing saving fig {end - start}")
-    plt.close(fig)
-    plt.clf()
+        plt.xlabel('JD', labelpad=TITLE_PAD)
+        # plt.ylabel("Absolute Mag, comp star = {:2.2f}".format(comparison_stars[0].vmag), labelpad=TITLE_PAD)
+        plot_max = curve_max + 0.1
+        plot_min = curve_min - 0.1
+        plt.ylim(plot_min, plot_max)
+        plt.xlim(curve['floatJD'].min(), curve['floatJD'].max())
+        plt.gca().invert_yaxis()
+        plt.ticklabel_format(style='plain', axis='x')
+        plt.title(f"Star {star_id}{vsx_title}\nposition: {utils.get_hms_dms_matplotlib(coord)}{upsilon_text}", pad=TITLE_PAD)
+        plt.tight_layout()
+        start = timer()
+        fig.savefig(save_location)
+        end = timer()
+        logging.debug(f"timing saving fig {end - start}")
+        plt.close(fig)
+        plt.clf()
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        logging.error(message)
+        logging.error(f"Error during plot lightcurve: {star.local_id}")
 
 
 def plot_phase_diagram(star: StarDescription, curve: DataFrame, fullphasedir, suffix='', period=None):
-    logging.debug(f"Starting plot phase diagram with {star} and {fullphasedir}")
-    coords = star.coords
-    star_id = star.local_id
-    vsx_name, _, filename_no_ext = get_star_or_catalog_name(star, suffix=f"_phase{suffix}")
-    vsx_title = f"{vsx_name}\n" if vsx_name is not None else ''
-    save_location = PurePath(fullphasedir, filename_no_ext+'.png')
-    upsilon_match = star.get_metadata('UPSILON')
-    upsilon_text = upsilon_match.get_upsilon_string() if upsilon_match is not None else ''
-    # print("Calculating phase diagram for", star)
-    if curve is None:
-        logging.info("Curve of star {} is None".format(star_id))
-        return
-    t_np = curve['floatJD']
-    y_np = curve['realV'].to_numpy()
-    dy_np = curve['realErr'].to_numpy()
-    if period is None:
-        period_max = np.max(t_np) - np.min(t_np)
-        if period_max <= 0.01:
+    try:
+        logging.debug(f"Starting plot phase diagram with {star} and {fullphasedir}")
+        coords = star.coords
+        star_id = star.local_id
+        vsx_name, _, filename_no_ext = get_star_or_catalog_name(star, suffix=f"_phase{suffix}")
+        vsx_title = f"{vsx_name}\n" if vsx_name is not None else ''
+        save_location = PurePath(fullphasedir, filename_no_ext+'.png')
+        upsilon_match = star.get_metadata('UPSILON')
+        upsilon_text = upsilon_match.get_upsilon_string() if upsilon_match is not None else ''
+        # print("Calculating phase diagram for", star)
+        if curve is None:
+            logging.info("Curve of star {} is None".format(star_id))
             return
-        ls = LombScargleFast(optimizer_kwds={'quiet': True, 'period_range': (0.01, period_max)},
-                             silence_warnings=True).fit(t_np, y_np)
-        period = ls.best_period
-        # TODO test this trended lombscargle !
-        # tmodel = TrendedLombScargle(optimizer_kwds={'quiet': True, 'period_range': (0.01, period_max)},
-        #                             silence_warnings=True).fit(t_np, y_np)
-        # period = tmodel.best_period
-    fig = plt.figure(figsize=(18, 16), dpi=80, facecolor='w', edgecolor='k')
-    plt.xlabel("Phase", labelpad=TITLE_PAD)
-    plt.ylabel("Magnitude", labelpad=TITLE_PAD)
-    plt.title(f"{vsx_title}Star {star_id}, p: {period:.5f} d{upsilon_text}\n{utils.get_hms_dms_matplotlib(coords)}",
-              pad=TITLE_PAD)
-    plt.ticklabel_format(style='plain', axis='x')
-    # plt.title(f"Star {star} - {period}", pad=TITLE_PAD)
-    plt.tight_layout()
-    # plotting + calculation of 'double' phase diagram from -1 to 1
-    phased_t = np.fmod(t_np / period, 1)
-    minus_one = np.vectorize(lambda t: t - 1)
-    phased_t2 = minus_one(phased_t)
-    phased_lc = y_np[:]
-    phased_t_final = np.append(phased_t2, phased_t)
-    phased_lc_final = np.append(phased_lc, phased_lc)
-    phased_err = np.clip(np.append(dy_np, dy_np), -0.5, 0.5)  # error values are clipped to +0.5 and -0.5
-    plt.gca().invert_yaxis()
-    plt.errorbar(phased_t_final, phased_lc_final, yerr=phased_err, linestyle='none', marker='o', ecolor='gray',
-                 elinewidth=1)
-    logging.debug(f"Saving phase plot to {save_location}")
-    fig.savefig(save_location, format='png')
-    plt.close(fig)
-    plt.clf()
-    with open(f"{fullphasedir}/txt/{filename_no_ext}.txt", 'w') as f:
-        f.write('\n'.join([f"period={period}", f'range="{np.min(y_np):.1f}-{np.max(y_np):.1f}"',
-                           f"coords=[{coords.ra.deg}, {coords.dec.deg}]"]))
+        t_np = curve['floatJD']
+        y_np = curve['realV'].to_numpy()
+        dy_np = curve['realErr'].to_numpy()
+        if period is None:
+            period_max = np.max(t_np) - np.min(t_np)
+            if period_max <= 0.01:
+                return
+            ls = LombScargleFast(optimizer_kwds={'quiet': True, 'period_range': (0.01, period_max)},
+                                 silence_warnings=True).fit(t_np, y_np)
+            period = ls.best_period
+            # TODO test this trended lombscargle !
+            # tmodel = TrendedLombScargle(optimizer_kwds={'quiet': True, 'period_range': (0.01, period_max)},
+            #                             silence_warnings=True).fit(t_np, y_np)
+            # period = tmodel.best_period
+        fig = plt.figure(figsize=(18, 16), dpi=80, facecolor='w', edgecolor='k')
+        plt.xlabel("Phase", labelpad=TITLE_PAD)
+        plt.ylabel("Magnitude", labelpad=TITLE_PAD)
+        plt.title(f"{vsx_title}Star {star_id}, p: {period:.5f} d{upsilon_text}\n{utils.get_hms_dms_matplotlib(coords)}",
+                  pad=TITLE_PAD)
+        plt.ticklabel_format(style='plain', axis='x')
+        # plt.title(f"Star {star} - {period}", pad=TITLE_PAD)
+        plt.tight_layout()
+        # plotting + calculation of 'double' phase diagram from -1 to 1
+        phased_t = np.fmod(t_np / period, 1)
+        minus_one = np.vectorize(lambda t: t - 1)
+        phased_t2 = minus_one(phased_t)
+        phased_lc = y_np[:]
+        phased_t_final = np.append(phased_t2, phased_t)
+        phased_lc_final = np.append(phased_lc, phased_lc)
+        phased_err = np.clip(np.append(dy_np, dy_np), -0.5, 0.5)  # error values are clipped to +0.5 and -0.5
+        plt.gca().invert_yaxis()
+        plt.errorbar(phased_t_final, phased_lc_final, yerr=phased_err, linestyle='none', marker='o', ecolor='gray',
+                     elinewidth=1)
+        logging.debug(f"Saving phase plot to {save_location}")
+        fig.savefig(save_location, format='png')
+        plt.close(fig)
+        plt.clf()
+        with open(f"{fullphasedir}/txt/{filename_no_ext}.txt", 'w') as f:
+            f.write('\n'.join([f"period={period}", f'range="{np.min(y_np):.1f}-{np.max(y_np):.1f}"',
+                               f"coords=[{coords.ra.deg}, {coords.dec.deg}]"]))
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        logging.error(message)
+        logging.error(f"Error during plot phase: {star.local_id}")
 
 
 def get_star_or_catalog_name(star: StarDescription, suffix: str):
@@ -229,54 +241,53 @@ def read_vast_lightcurves(star: StarDescription, ns, do_charts, do_phase, do_aav
         f"Reading lightcurves for star {star.local_id} at path {star.path} for {star}...")
     # comp_mags = [x.vmag for x in comparison_stars]
 
-    # try:
-    df = pd.read_csv(star.path, delim_whitespace=True,
-                     names=['JD', 'Vrel', 'err', 'X', 'Y', 'unknown', 'file', 'vast1', 'vast2', 'vast3', 'vast4',
-                            'vast5', 'vast6', 'vast7', 'vast8', 'vast9', 'vast10'], dtype={'JD': str})
+    try:
+        df = pd.read_csv(star.path, delim_whitespace=True,
+                         names=['JD', 'Vrel', 'err', 'X', 'Y', 'unknown', 'file', 'vast1', 'vast2', 'vast3', 'vast4',
+                                'vast5', 'vast6', 'vast7', 'vast8', 'vast9', 'vast10'], dtype={'JD': str})
 
-    if df is None or len(df) == 0:
-        logging.info(f"No lightcurve found for {star.path}")
-        return
+        if df is None or len(df) == 0:
+            logging.info(f"No lightcurve found for {star.path}")
+            return
 
-    filtered_compstars = do_compstars.get_star_compstars_from_catalog(star, ns.comp_stars)
-    df['realV'], df['realErr'] = do_compstars.calculate_ensemble_photometry(df, filtered_compstars,
-                                                                            do_compstars.weighted_value_ensemble_method)
-    df['floatJD'] = df['JD'].astype(np.float)
-    filtered_compstars = None
-    if do_charts:
-        # start = timer()
-        # logging.debug("NO LICGHTCRUVEGYET ")
-        plot_lightcurve_jd(star, df.copy(), chartsdir)
-        # end = timer()
-        # print("plotting lightcurve:", end-start)
-    if do_phase:
-        if star.has_metadata("STARFILE") and star.get_metadata("STARFILE") is None:
-            # This should not happen!!!
-            logging.warning(f"This should not happen !!!!!!!!!!!!!!!!!!!! "
-                            f"{star.local_id}, {star.get_metadata_list()}, {star.has_metadata('STARFILE')}, "
-                            f"{[star.get_metadata(x) for x in star.get_metadata_list()]}")
+        filtered_compstars = do_compstars.get_star_compstars_from_catalog(star, ns.comp_stars)
+        df['realV'], df['realErr'] = do_compstars.calculate_ensemble_photometry(df, filtered_compstars,
+                                                                                do_compstars.weighted_value_ensemble_method)
+        df['floatJD'] = df['JD'].astype(np.float)
+        filtered_compstars = None
+        if do_charts:
+            # start = timer()
+            # logging.debug("NO LICGHTCRUVEGYET ")
+            plot_lightcurve_jd(star, df.copy(), chartsdir)
+            # end = timer()
+            # print("plotting lightcurve:", end-start)
+        if do_phase:
+            if star.has_metadata("STARFILE") and star.get_metadata("STARFILE") is None:
+                # This should not happen!!!
+                logging.warning(f"This should not happen !!!!!!!!!!!!!!!!!!!! "
+                                f"{star.local_id}, {star.get_metadata_list()}, {star.has_metadata('STARFILE')}, "
+                                f"{[star.get_metadata(x) for x in star.get_metadata_list()]}")
 
-        if star.get_metadata("STARFILE") is not None:
-            period = star.get_metadata("STARFILE").period
-            logging.debug(f"Using provided period for star {star.local_id}: {period}")
-            plot_phase_diagram(star, df.copy(), phasedir, period=period)
-        else:
-            plot_phase_diagram(star, df.copy(), phasedir, period=None, suffix="")
-    if do_aavso:
-        # TODO put this in settings.txt
-        sitelat = '-22 57 10'
-        sitelong = '-68 10 49'
-        sitealt = 2500
-        observer = 'ZZZ'
-        do_aavso_report.report(star, df.copy(), target_dir=aavsodir, vastdir=basedir, sitelat=sitelat,
-                               sitelong=sitelong, sitealt=sitealt, comparison_stars=ns.comp_stars, filter='V',
-                               observer=observer, chunk_size=aavso_limit)
-
-    # except Exception as ex:
-    #     template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-    #     message = template.format(type(ex).__name__, ex.args)
-    #     logging.error(message)
-    #     logging.error(f"Exception during read_lightcurve for {star.path}")
+            if star.get_metadata("STARFILE") is not None:
+                period = star.get_metadata("STARFILE").period
+                logging.debug(f"Using provided period for star {star.local_id}: {period}")
+                plot_phase_diagram(star, df.copy(), phasedir, period=period)
+            else:
+                plot_phase_diagram(star, df.copy(), phasedir, period=None, suffix="")
+        if do_aavso:
+            # TODO put this in settings.txt
+            sitelat = '-22 57 10'
+            sitelong = '-68 10 49'
+            sitealt = 2500
+            observer = 'ZZZ'
+            do_aavso_report.report(star, df.copy(), target_dir=aavsodir, vastdir=basedir, sitelat=sitelat,
+                                   sitelong=sitelong, sitealt=sitealt, comparison_stars=ns.comp_stars, filter='V',
+                                   observer=observer, chunk_size=aavso_limit)
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        logging.error(message)
+        logging.error(f"Exception during read_lightcurve for {star.path}")
 
     end = timer()
     logging.debug(f"Full lightcurve/phase: {end - start}")
