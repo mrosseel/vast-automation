174c45e734268e95b8f15dcc97e0b9b3ce388246
Mike Rosseel <mike.rosseel@gmail.com>
2019-11-04 16:22:16 +0100

    Set logging to info for first run

--- src/do_charts_vast.py
+++ src/do_charts_vast.py
@@ -260,7 +260,7 @@ def read_vast_lightcurves(star: StarDescription, compstarproxy, do_charts, do_ph
         old_size = len(df)
         # remove errors
         df = reject_outliers_iqr(df, 'realV', 0.02)
-        logging.debug(f"Rejected {old_size-len(df)} observations with iqr.")
+        logging.info(f"Rejected {old_size-len(df)} observations with iqr.")
         if do_charts:
             # start = timer()
             # logging.debug("NO LIGHTCRUVEGYET ")
