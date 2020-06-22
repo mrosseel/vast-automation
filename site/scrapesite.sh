9af858b58a59da15e81d489ca51b702dfbf8794c
Mike Rosseel <mike.rosseel@gmail.com>
2020-06-22 06:54:57 +0000

    Added comment to scrapesite

--- site/scrapesite.sh
+++ site/scrapesite.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+# gets an exact copy of one hugo post. Afterwards, replace the top-level index.html with a symlink to the post index.html
 wget --no-clobber --convert-links --random-wait -r -p --level 1 -E -e robots=off -U mozilla $1
