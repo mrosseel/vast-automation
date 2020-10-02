3a0ccff3328d100b29f04079196a23e628039487
Mike Rosseel <mike.rosseel@gmail.com>
2020-10-02 17:26:11 +0200

    Fixed csv export preamble with filenames. Added ucac4 name.

--- site/scrapesite.sh
+++ site/scrapesite.sh
@@ -2,5 +2,5 @@
 # gets an exact copy of one hugo post. Afterwards, replace the top-level index.html with a symlink to the post index.html
 wget --no-clobber --convert-links --random-wait -r -p --level 1 -E -e robots=off -U mozilla $1
 python3 process_site.py $1
-echo "Now run find . -type f -name "*.png" -exec optipng -o5 {} \;"
+# echo "Now run find . -type f -name "*.png" -exec optipng -o5 {} \;"
 
--- src/main_vast.py
+++ src/main_vast.py
@@ -371,7 +371,7 @@ def write_selected_files(
             f"other stars: {no_vsx_len}\n"
         )
         outowncatalog.write(
-            f"{preamble}# our_name,ra,dec,ucac4_name,ucac4_ra,ucac4_dec,minmax,min,max,var_type,"
+            f"{preamble}# our_name,ra,dec,ucac4_name,ucac4_ra,ucac4_dec,ucac4_force,minmax,min,max,var_type,"
             f"period,period_err,epoch\n"
         )
         outselected.write(
@@ -422,7 +422,7 @@ def write_selected_files(
                     f"{format_string('epoch', parsed_toml)}\n"
                 )
                 outselected.write(
-                    f"{metadata.our_name},{star.local_id},,False,"
+                    f"{metadata.our_name},{star.local_id},{star.ucac4_name},False,"
                     f"{format_string('minmax', parsed_toml)},{format_float_1(parsed_toml, 'min')},"
                     f"{format_float_1(parsed_toml, 'max')},{metadata.var_type},"
                     f"{format_float_5(parsed_toml, 'period')},{format_float_5(parsed_toml, 'period_err')},"
